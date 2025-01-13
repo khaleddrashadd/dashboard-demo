2c0efe827efc67f515060f78ed87e38e7da4646b
Khaled Rashad <khaledrashadd@outlook.com>
2025-01-13 11:22:34 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,15 @@ export default defineConfig({
   plugins: [
     react(),
     svgr({
+      // SVGR options
+      svgrOptions: {
+        // icon: true, // Optional - if you're using icons
+        exportType: 'default', // Use default export
+        ref: true, // Optional - if you need ref support
+        svgo: true, // Optimize SVGs
+        titleProp: true, // Add title prop support
+      },
+      // Include files ending in ?react
       include: '**/*.svg?react',
     }),
   ],
