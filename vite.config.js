ee49ef34faa6496dffa2be185b83174d048b4c7e
Khaled Rashad <khaledrashadd@outlook.com>
2025-01-13 11:00:42 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,20 @@ import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [
+    react(),
+    svgr({
+      exportAsDefault: true,
+      svgrOptions: {
+        icon: true,
+        svgo: true,
+        plugins: ['@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': '/src', // If you're using path aliases
+    },
+  },
 });
