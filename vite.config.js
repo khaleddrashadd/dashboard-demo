d8d067e04a033ea582b265b56a6c482ea04667b6
Khaled Rashad <khaledrashadd@outlook.com>
2025-02-02 12:30:06 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import svgr from 'vite-plugin-svgr';
 import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr({
+      // SVGR options
+      svgrOptions: {
+        // icon: true, // Optional - if you're using icons
+        exportType: 'default', // Use default export
+        ref: true, // Optional - if you need ref support
+        svgo: true, // Optimize SVGs
+        titleProp: true, // Add title prop support
+      },
+      // Include files ending in ?react
+      include: '**/*.svg?react',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
